ee0f086a73fc94fd673bc0567e3508ef79d2d029
papajoker <papajoke@manjaro.fr>
2019-03-20 20:15:28 +0100

    filter tests (bad) speed

--- pacman-logs.py
+++ pacman-logs.py
@@ -125,9 +125,29 @@ class AlpmLog():
     def list_actions(cls):
         return cls.actions.keys()
 
-
-
-
+    def filters(self, aname="", adate="", aaction=""):
+        """
+        filter name, date or action
+        """
+        for item in self.items:
+            result = True
+            if aname:
+                if aname.endswith(" "):
+                    result = aname == item['pkg']+" "
+                    if not result:
+                        continue
+                else:
+                    result = aname in item['pkg']
+                    if not result:
+                        continue
+            if adate:
+                result = aname in item['date'] # convert to yy-mm-dd
+                if not result:
+                    continue
+            if aaction:
+                result = aaction == item['verb']
+            if result:
+                yield item
 
 
 class CalDialog(Gtk.Dialog):
@@ -156,6 +176,7 @@ class MainApp:
     def __init__(self, classlog: AlpmLog):
         """main app window"""
         self.alpm = classlog
+        self.filter = None
         builder = Gtk.Builder()
         builder.add_from_file('pacman-logs.glade')
         window = builder.get_object('window')
@@ -235,13 +256,13 @@ class MainApp:
         print('bgcolor ACTIVE: ', color, color.to_string())
 
         if bgcolor.red < 0.5 and bgcolor.blue < 0.5:
-            print ("dark theme")
+            print("dark theme")
         else:
-            print ("light theme")
+            print("light theme")
 
+        self.title = builder.get_object('headerbar')
         self.store = builder.get_object('logstore')
         self.init_logs()
-        builder.get_object('headerbar').set_title(f"Pacman Logs - {len(self.store)}")
         window.show_all()
         #builder.get_object('home').hide()
         builder.get_object('verbs').hide()
@@ -261,6 +282,11 @@ class MainApp:
 
         self.create_actions()
 
+    def set_title(self):
+        nb = len(self.store)
+        if self.filter:
+            nb = len(self.filter)
+        self.title.set_title(f"Pacman Logs - {nb}")
 
 
     def create_actions(self):
@@ -300,7 +326,7 @@ class MainApp:
         action.connect('activate', self.pop_action, self.alpm.ACTION, "warning")
         self.actions.add_action(action)
 
-        action = Gtk.Action(name='filter_none', label="no filter", tooltip=None, stock_id=None)
+        action = Gtk.Action(name='filter_none', label="no filter", tooltip=None, stock_id=Gtk.STOCK_HOME)
         action.connect('activate', self.on_raz)
         self.actions.add_action(action)
 
@@ -390,7 +416,17 @@ class MainApp:
         if action == self.alpm.ACTION:
             # filter by action ...
             self.filter_action = text
+            # TODO
+            #très très lent !!!!!
+            #tester si possible de dévalider le tri puis remettre après filtre
+            #replace filter.refilter() by 
+            # - treeview_populate
+            # - or treeview.line.visible = false ??? je ne pense pas
+            print('filter.refilter()', 'begin')
             self.filter.refilter()
+            #self.populate()
+            print('filter.refilter()', 'END')
+            self.set_title()
 
     def on_raz(self, btn):
         self.entry.set_text('')
@@ -398,6 +434,8 @@ class MainApp:
         self.filter_action = None
         #TODO only if i change a value
         self.filter.refilter()
+        #self.populate()
+        self.set_title()
         self.stack.set_visible_child_name("page0")
 
     def on_drag_data_get(self, treeview, context, selection, target_id, etime):
@@ -423,10 +461,36 @@ class MainApp:
             #self.entry.set_focus(True)
         #self.entry.connect('search-changed', self.on_search_changed)
 
+    def populate(self):
+        self.store.clear()
+        items = list(self.alpm.filters( adate=str(self.entryd.props.text), aname=str(self.entry.props.text).lower(), aaction=self.filter_action))
+        for item in self.alpm.items:
+            if item['verb'] != 'transaction':
+                msg = ''
+                warning = ''
+                if item['verb'] == 'warning':
+                    msg = item.get('msg')
+                    if msg.endswith('pacnew'):
+                        warning = ' (pacnew)'
+                    if msg.endswith('pacsave'):
+                        warning = ' (pacsave)'
+                    if 'directory permissions' in msg:
+                        warning = ' (chmod)'
+                self.store.append([
+                    str(item['date'])[:-3],
+                    item['verb'],
+                    item['pkg'],
+                    item['ver'] + warning,
+                    "" + self.alpm.actions.get(item['verb'], 'home'),
+                    item['date'].strftime('%c').split(' ', 1)[1][:-4],      # local format date
+                    msg,
+                    item['l']
+                ])
+
     def init_logs(self):
         """ set datas"""
         # set logstore ... //GtkListStore
-        self.store.clear()
+        #self.store.clear()
         column = Gtk.TreeViewColumn('Date', Gtk.CellRendererText(), text=5)
         column.set_resizable(True)
         column.set_reorderable(True)
@@ -437,7 +501,6 @@ class MainApp:
         renderer = Gtk.CellRendererText()
         renderer.set_alignment(0.5, 0.5)
         column = Gtk.TreeViewColumn('Action', renderer, text=4)
-        #column = Gtk.TreeViewColumn('Action', Gtk.CellRendererPixbuf(), icon_name=4)
         column.set_resizable(False)
         column.set_reorderable(True)
         column.set_sort_column_id(self.alpm.ACTION)
@@ -451,29 +514,9 @@ class MainApp:
         column.set_resizable(True)
         self.treeview.append_column(column)
 
-        for item in self.alpm.items:
-            if item['verb'] != 'transaction':
-                msg = ''
-                warning = ''
-                if item['verb'] == 'warning':
-                    msg = item.get('msg')
-                    if msg.endswith('pacnew'):
-                        warning = ' (pacnew)'
-                    if msg.endswith('pacsave'):
-                        warning = ' (pacsave)'
-                    if msg.startswith('directory permissions'):
-                        warning = ' (chmod)'
-                self.store.append([
-                    str(item['date'])[:-3],
-                    item['verb'],
-                    item['pkg'] + warning,
-                    item['ver'],
-                    "" + self.alpm.actions.get(item['verb'], 'home'),
-                    item['date'].strftime('%c').split(' ', 1)[1][:-4],      # local format date
-                    msg,
-                    item['l']
-                ])
-        self.alpm.items.clear()
+        self.populate()
+        self.alpm.items.clear() # no re-use populate
+        self.set_title()
 
         self.filter = self.store.filter_new()
         self.filter.set_visible_func(self.filter_func)
@@ -484,6 +527,8 @@ class MainApp:
         """Tests if pkg name or date in the row"""
         current_filter = str(self.entry.props.text).lower()
         result = True
+        # test if speed NUT NOT with > 10 000 items !!!
+        #return True
         if current_filter:
             if current_filter.endswith(" "):
                 result = current_filter == model[iter][self.alpm.PKG]+" "
@@ -549,15 +594,13 @@ class MainApp:
         Gtk.main()
 
     def on_search_changed(self, entry):
-        if entry == self.entry:
-            txt = self.entry.get_text()
-            if txt and len(txt) < 3:
-                return
-        if entry == self.entryd:
-            txt = self.entryd.get_text()
-            if txt and len(txt) < 2:
-                return
-        self.filter.refilter()
+        l = len(self.entry.get_text())
+        if l != 1:
+            print('filter.refilter()', 'begin')
+            self.filter.refilter()
+            #self.populate()
+            print('filter.refilter()', 'END')
+            self.set_title()
 
     def on_date_clicked(self, btn):
         """Open calender and get user selection"""
